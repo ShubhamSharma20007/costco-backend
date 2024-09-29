a5599197a24e0ffb3f52299b3c95d215e81d3718
shubham <shubhamsharma20007@gmail.com>
2024-09-29 23:08:37 +0530

    changes

--- db/connection.js
+++ db/connection.js
@@ -2,9 +2,7 @@ const mongoose =  require('mongoose')
 
 const connectionDB =()=>{
     try {
-     mongoose.connect(process.env.MONGO_URL,{
-        useNewUrlParser:true
-     })
+     mongoose.connect(process.env.MONGO_URL)
      .then(res=>console.log('connected ✔️'))
     .catch(err=>console.log(err))
         
